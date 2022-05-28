79100e712fb4cc4fd230c7fc982b58ef260fed7f
AlefoElfo <alfa_gama_32@hotmail.com>
2022-05-27 22:30:12 -0500

    Algoritmo

--- Instrucciones.py
+++ Instrucciones.py
@@ -1,4 +1,27 @@
-#Agregar JSON
-#Agregar pregunta
-#Comparar con jsonTesauro
-#Resultados
+#ALGORITMO
+
+# Digitalizar término statenvertaling[1]
+    # Relaciones semánticas del lexema[1]
+    # Definición[1]
+    # Meterlos en formato json[1]
+# (2a versión) Digitalizar 5 términos[0]
+    # Hacer tesauro manual[0]
+    # Agregar definición[0]
+    # Meterlos en formato json[0]
+
+# Pasarlos por pandas[0]
+
+# Pasarlos por spacy.nlp[0]
+    # Convertir a lemmas[0]
+    # Eliminar palabras innecesarias[0]
+
+# Hacer input de pregunta natural[0]
+    # pasar pregunta por spacy.nlp[0]
+    # Eliminar palabras innecesarias[0]
+    # Convertir a lemmas[0]
+
+# Contar coincidencias pregunta a términos[0]
+    # Ciclo for para contar[0]
+    # Ordenar por orden descendente de conincidencias[0]
+
+# Mostrar al usuario el término con mayores coincidencias[0]
