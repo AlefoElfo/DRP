395c09877f2eb48f9faeb9fabe16d4e78f65ad35
AlefoElfo <alfa_gama_32@hotmail.com>
2022-05-28 00:28:41 -0500

    Mejora visual algoritmo e instalación Spacy local

--- Instrucciones.py
+++ Instrucciones.py
@@ -1,21 +1,19 @@
-#ALGORITMO
+# [1] Digitalizar término statenvertaling
+    # [1]Relaciones semánticas del lexema
+    # [1]Definición
+    # [1]Meterlos en formato json
+# [0] (2a versión) Digitalizar 5 términos
+    # [0]Hacer tesauro manual
+    # [0]Agregar definición
+    # [0]Meterlos en formato json
 
-# Digitalizar término statenvertaling[1]
-    # Relaciones semánticas del lexema[1]
-    # Definición[1]
-    # Meterlos en formato json[1]
-# (2a versión) Digitalizar 5 términos[0]
-    # Hacer tesauro manual[0]
-    # Agregar definición[0]
-    # Meterlos en formato json[0]
-
-#JSON en Python[0]
-    # Instalar pandas[0] 
-        # No sirve porque pandas pide el mismo número de filas en archivos json. Lo dejo por si luego se resuelve
-    # Importar pandas, json[1] 
-        # Sólo JSON
-    # Meterlo a python como diccionario [1]
-    # Pasarlos por pandas[1] No sirve.
+# [0] JSON en Python
+    # [0] Instalar pandas
+        # (No sirve porque pandas pide el mismo número de filas en archivos json. Lo dejo por si luego se resuelve)
+    # [1] Importar pandas, json
+        # (Sólo JSON)
+    # [1] Meterlo a python como diccionario 
+    # [1] Pasarlos por pandas (No sirve)
 
 import json
 statenFile = open('D:\OneDrive\Proyectos\DRP\Statenvertaling.json', 'r', encoding='utf-8')
@@ -23,18 +21,29 @@ staten = json.load(statenFile)
 statenFile.close()
 type(staten)
 
-# Pasarlos por spacy.nlp[0]
-    # Instalar spaCy
-    # Convertir a lemmas[0]
-    # Eliminar palabras innecesarias[0]
+# [0] Importar spacy
+    # [1] Instalar spaCy
+        # [1] En anaconda prompt (base)
+        # [1] Instalar modelo de español profundo
+        # [1] Ver que el intérprete sea el mismo (python 3.9.7 base:conda)
+    # [1] Importar spacy
 
-# Hacer input de pregunta natural[0]
-    # pasar pregunta por spacy.nlp[0]
-    # Eliminar palabras innecesarias[0]
-    # Convertir a lemmas[0]
 
-# Contar coincidencias pregunta a términos[0]
-    # Ciclo for para contar[0]
-    # Ordenar por orden descendente de conincidencias[0]
+import spacy
+spacy.load('es_dep_news_trf')
 
-# Mostrar al usuario el término con mayores coincidencias[0]
+# [0] Pasarlos por spacy.nlp
+    # [0] Convertir a lemmas
+    # [0] Eliminar palabras innecesarias
+
+
+# [0] Hacer input de pregunta natural
+    # [0] pasar pregunta por spacy.nlp
+    # [0] Eliminar palabras innecesarias
+    # [0] Convertir a lemmas
+
+# [0] Contar coincidencias pregunta a términos
+    # [0] Ciclo for para contar
+    # [0] Ordenar por orden descendente de conincidencias
+
+# [0] Mostrar al usuario el término con mayores coincidencias
