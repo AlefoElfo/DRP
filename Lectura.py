fe329e43a55d13fda86e1627aa29fd0d24d3b851
AlefoElfo <alfa_gama_32@hotmail.com>
2022-06-01 22:39:35 -0500

    Termina alfa de "Lectura" y se separa en "Lectura" y "Pregunta"

--- Lectura.py
+++ Lectura.py
@@ -1,4 +1,4 @@
-# [1] Digitalizar término statenvertaling
+# [1] CREAR TESAURO EN JSON
     # [1]Relaciones semánticas del lexema
     # [1]Definición
     # [1]Meterlos en formato json
@@ -11,18 +11,26 @@
 
 
 
-# [1] JSON en Python
+# [1] JSON EN PYTHON
     # [0] Instalar pandas
         # (No sirve porque pandas pide el mismo número de filas en archivos json. Lo dejo por si luego se resuelve)
     # [0] Importar pandas
     # [1] Importar json
     # [1] Meterlo a python como diccionario 
+        # (Con utf-8, por los acentos y todo)
     # [1] Pasarlos por pandas (No sirve)
+# [1] Preparar texto
+    # [1] Quitar los nombres y sólo dejar los valores
+    # [1] Convertir a atring
+
 
 import json
 with open('Statenvertaling.json', 'r', encoding='utf-8') as statenFile:
     staten = json.load(statenFile)
-    print('Archivo JSON recibido con', len(staten), 'atributos:')
+    print('''-------
+Archivo JSON ingresado como DICT''')
+    print('Con', len(staten), 'atributos:')
+    print('-------')
 for key,value in staten.items():
     print(key)
     statenValue = (str(value))
@@ -32,60 +40,48 @@ for key,value in staten.items():
 
 
 
+# [1] LIMPIAR STRING
+    # [1] Eliminar residuos de json
+    # [1] Hacer minúsculas todas las palabras
+
+residuosJSON = '''[]{}""':,'''
+replace = ''
+for str in statenValue:
+    if str in residuosJSON:
+        statenValue = statenValue.replace(str, '')
+statenLower = statenValue.lower()
+print('''-------
+Archivo DICT se ha limpiado
+-------''')
+
+
+
+
+
+
 # [1] Importar spacy
     # [1] Instalar spaCy
         # [1] En anaconda prompt (base)
         # [1] Instalar modelo de español profundo
+            # (pip install -U spacy[cuda110]) sin las comillas para GPU
+            # Instalar CUDA para la GPU
         # [1] Ver que el intérprete sea el mismo (python 3.9.7 base:conda)
     # [1] Importar spacy
-    # [1] Añadir 
-
-# ESTE CÓDIGO FUNCIONA, PERO NO PUEDO GENERAR UNA VARIABLE CON EL RESULTADO FINAL
-# import spacy
-# nlp = spacy.load('es_dep_news_trf')
-# statenNLP = nlp(statenValue)
-# print('Archivo analizado por IA')
-# for token in statenNLP:
-#     if token.is_alpha:
-#         print(token.text)
-
+        # [1] Añadir 
+    # [0] Pasarlos por spacy.nlp
+        # [0] Eliminar palabras repetidas
+        # [0] Saltar stop words (me,te,el,las,los)
+        # [0] Convertir a lemmas
 
 import spacy
 nlp = spacy.load('es_dep_news_trf')
-statenNLP = nlp(statenValue)
-print('Archivo analizado por IA')
+statenNLP = (set(nlp(statenLower)))
+statenLemma = []
 for token in statenNLP:
-    if token.is_alpha:
-        print(token.text)
-
-# [0] Pasarlos por spacy.nlp
-    # [0] Convertir a lemmas
-    # [0] Eliminar palabras innecesarias
-
-
-# [0] Preparar texto (No es tan necesario porque es texto introducido manualmente)
-# [0] Limpiar texto con una función
-# [0] Quitar los nombres y sólo dejar los valores
-
-# with staten as statenKey:
-#    staten.items
-# for key in staten.items():
-#    print(key, ":", value)
-
-
-# [0] Hacer input de pregunta natural
-    # [0] pasar pregunta por spacy.nlp
-    # [0] Eliminar palabras innecesarias
-    # [0] Convertir a lemmas
-
-# [0] Crear función para leer los json
-    # [0] For lectura in jsonFIle:
-    # [0] buscar en nombre, hiperónimos, cohipónimos, hipónimos, polisemia, antonimia, meronimia, definición
-    # [0] Contar coincidencias de acuerdo con la pregunta
-    # [0] Arrojar resultado de coincidencias
-
-# [0] Contar coincidencias pregunta a términos
-    # [0] Ciclo for para contar
-    # [0] Ordenar por orden descendente de conincidencias
-
-# [0] Mostrar al usuario el término con mayores coincidencias
+    if not token.is_stop:
+        statenLemma.append(token.lemma_)
+print('''-------
+Archivo DICT analizado por PLN
+-------''')
+# statenAlone = set(statenNLP)
+print(statenLemma)
---
+++ Pregunta.py
@@ -0,0 +1,41 @@
+# [0] Hacer input de pregunta natural
+    # [0] Pedir pregunta al usuario
+    # [0] Convenrtir a string
+    # [0] Limpiar puntuación
+    # [0] Volver minúsculas
+    # [0] pasar pregunta por spacy.nlp
+    # [0] Eliminar palabras innecesarias
+    # [0] Convertir a lemmas
+
+
+print('''¡Hola! ¿No recuerdas el término?
+¿Lo tienes en la punta de la lengua?
+Escribe lo que recuerdes y buscaré la mejor opción:
+''')
+pregunta = str(input())
+
+#LIMPIAR PREGUNTA
+   
+   
+preguntaPunct = '[!¡"\#¿?()=,.-_:;]'
+replace = ''
+for str in pregunta:
+    if str in preguntaPunct:
+        pregunta = pregunta.replace(str, '')
+preguntaLower = pregunta.lower()
+
+print(preguntaLower)
+
+
+
+# [0] Crear función para leer los json
+    # [0] For lectura in jsonFIle:
+    # [0] buscar en nombre, hiperónimos, cohipónimos, hipónimos, polisemia, antonimia, meronimia, definición
+    # [0] Contar coincidencias de acuerdo con la pregunta
+    # [0] Arrojar resultado de coincidencias
+
+# [0] Contar coincidencias pregunta a términos
+    # [0] Ciclo for para contar
+    # [0] Ordenar por orden descendente de conincidencias
+
+# [0] Mostrar al usuario el término con mayores coincidencias
